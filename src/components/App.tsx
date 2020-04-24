15c07b3a85742e473c03d80328d5bdec654f5360
marina <marina.vrataric@gmail.com>
2020-04-24 14:58:04 +0200

    removed <StrictMode> from index.js which caused disabled checkbox and added functionality ADD_TODO, CHECK_TODO

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import React, { useRef, useReducer } from 'react'
 
 function App() {
+
+    const inputRef = useRef<HTMLInputElement>(null)
+
+    const handleSubmit = (e: any) => {
+        e.preventDefault()
+        inputRef.current?.value !== "" && dispatch({ type: 'ADD_TODO', payload: inputRef.current?.value })
+        inputRef.current && (inputRef.current.value = "")
+    }
+
+    const [todo, dispatch] = useReducer((state: any, action: any): any => {
+        switch (action.type) {
+            case 'ADD_TODO':
+                return [...state, { id: state.length, name: action.payload, isCheck: false }]
+            case 'CHECK_TODO':
+                return state.filter((item: any, index: any):any => {
+                    if (index === action.id) {
+                        item.isCheck = !item.isCheck
+                    }
+                    return item
+                })
+        }
+    }, [])
+
+    const todos = todo.map((item: any, index: number) => {
+        return (
+            <li key={index}>
+                <input type="checkbox" checked={item.isCheck} onChange={() => dispatch({ type: "CHECK_TODO", id: index })} />
+                {item.name}
+            </li>
+        )
+    })
+
     return (
         <div>
-            
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    placeholder="Buy milk"
+                    ref={inputRef}
+                />
+            </form>
+            <ul>{todos}</ul>
         </div>
     )
 }
